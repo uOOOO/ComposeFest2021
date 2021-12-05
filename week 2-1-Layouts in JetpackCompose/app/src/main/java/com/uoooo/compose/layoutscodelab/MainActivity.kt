7a7ccf05e9366699cc04aac3ded88316b7f210d5
SeungHun Choe <13363349+uOOOO@users.noreply.github.com>
2021-12-06 03:49:30 +0900

    LayoutsCodelab Layout modifiers under the hood

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/uoooo/compose/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/uoooo/compose/layoutscodelab/MainActivity.kt
@@ -3,12 +3,16 @@ package com.uoooo.compose.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.uoooo.compose.layoutscodelab.ui.theme.LayoutsCodelabTheme
@@ -54,11 +58,18 @@ fun LayoutsCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    StaggeredGrid(modifier = modifier) {
-        for (topic in topics) {
-            Chip(modifier = Modifier.padding(8.dp), text = topic)
-        }
-    }
+    Row(modifier = modifier
+        .background(color = Color.LightGray)
+        .padding(16.dp)
+        .size(200.dp)
+        .horizontalScroll(rememberScrollState()),
+        content = {
+            StaggeredGrid {
+                for (topic in topics) {
+                    Chip(modifier = Modifier.padding(8.dp), text = topic)
+                }
+            }
+        })
 }
 
 @Preview
